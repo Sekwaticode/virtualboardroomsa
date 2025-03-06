d9b134c0bb697819552f111eb03e46aec510824b
Sekwaticode <lsekwati2@gmail.com>
2025-03-07 00:20:08 +0200

    some hero animations

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { FC, useEffect } from "react";
 import heroImage from "@/assets/images/hero-image.jpg";
@@ -15,16 +15,20 @@ const Hero: FC = () => {
       types: "lines,words",
       tagName: "span",
     });
-    titleAnimate(titleScope.current.querySelectorAll('.word'), {transform: 'translateY(0)'},
-    {
-      duration: .5,
-      delay: stagger(.2)
-    })
+    titleAnimate(
+      titleScope.current.querySelectorAll(".word"),
+      { transform: "translateY(0)" },
+      {
+        duration: 0.5,
+        delay: stagger(0.2),
+      }
+    );
   }, []);
 
   return (
     <section>
-      <div className="grid md:grid-cols-12 md:h-screen items-stretch">
+      <div className="grid md:grid-cols-12 md:h-screen items-stretch sticky top-0"></div>
+       <div className="h-[200vh] border-4 border-red-500">
         <div className="md:col-span-7 flex flex-col justify-center">
           <div className="container !max-w-full">
             <motion.h1
@@ -36,28 +40,58 @@ const Hero: FC = () => {
               Crafting digital experiences through code and creative design
             </motion.h1>
             <div className="flex flex-col md:flex-row md:items-center items-start mt-10 gap-6">
-              <Button
-                variant="secondary"
-                iconAfter={
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="size-5"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
-                    />
-                  </svg>
-                }
+              <motion.div
+                initial={{ opacity: 0, y: "100%" }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{duration: 0.5, delay: 1.75}}
               >
-                <span>View My Work</span>
-              </Button>
-              <Button variant="text">Let&apos;s Talk</Button>
+                <Button
+                  variant="secondary"
+                  iconAfter={
+                    <div className="overflow-hidden size-5">
+                    <div className="h-5 w-10 flex group-hover/button:-translate-x-1/2 transition-transform duration-500">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="size-5"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
+                      />
+                    </svg>
+                    </div>
+                     <svg
+                     xmlns="http://www.w3.org/2000/svg"
+                     fill="none"
+                     viewBox="0 0 24 24"
+                     strokeWidth="1.5"
+                     stroke="currentColor"
+                     className="size-5"
+                   >
+                     <path
+                       strokeLinecap="round"
+                       strokeLinejoin="round"
+                       d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
+                     />
+                   </svg>
+                   </div>
+                  }
+                >
+                  <span>View My Work</span>
+                </Button>
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0, y: "100%" }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{duration: 0.5, delay: 2.2}}
+              >
+                <Button variant="text">Let&apos;s Talk</Button>
+              </motion.div>
             </div>
           </div>
         </div>
