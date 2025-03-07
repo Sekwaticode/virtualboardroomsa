3ab1bc38be3106824e63fe70ba61509dd1daf824
Sekwaticode <lsekwati2@gmail.com>
2025-03-08 00:04:24 +0200

    boxgrid improvements

--- src/components/BoxGrid.js
+++ src/components/BoxGrid.js
@@ -1,43 +1,52 @@
-'use client'
+"use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./BoxGrid.module.css";
 
 const BoxGrid = () => {
+  const boxRefs = useRef([]);
+
   const boxes = [
     { title: "Title One", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, quae?" },
     { title: "Title Two", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, quae?" },
     { title: "Title Three", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, quae?" },
     { title: "Title Four", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, quae?" },
-
+    { title: "Title Five", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, quae?" },
+    { title: "Title Six", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, quae?" },
   ];
 
   useEffect(() => {
-    const handleMouseMove = (e) => {
-      document.querySelectorAll(".box").forEach((box) => {
-        let x = e.pageX - box.offsetLeft;
-        let y = e.pageY - box.offsetTop;
+    const handleMouseMove = (e, box) => {
+      const rect = box.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
 
-        box.querySelector("span").style.left = x + "px";
-        box.querySelector("span").style.top = y + "px";
-      });
+      const span = box.querySelector("span");
+      if (span) {
+        span.style.left = `${x}px`;
+        span.style.top = `${y}px`;
+      }
     };
 
-    document.querySelectorAll(".box").forEach((box) => {
-      box.addEventListener("mousemove", handleMouseMove);
+    boxRefs.current.forEach((box) => {
+      if (box) {
+        const moveHandler = (e) => handleMouseMove(e, box);
+        box.addEventListener("mousemove", moveHandler);
+
+        // Cleanup function
+        return () => box.removeEventListener("mousemove", moveHandler);
+      }
     });
-
-    return () => {
-      document.querySelectorAll(".box").forEach((box) => {
-        box.removeEventListener("mousemove", handleMouseMove);
-      });
-    };
   }, []);
 
   return (
     <div className={styles.container}>
       {boxes.map((box, index) => (
-        <div key={index} className={`${styles.box} box`}>
+        <div
+          key={index}
+          ref={(el) => (boxRefs.current[index] = el)}
+          className={`${styles.box} box`}
+        >
           <div className={styles.content}>
             <h2>{box.title}</h2>
             <p>{box.content}</p>
